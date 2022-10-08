7505642512fbdd4c14a802fd2635ca8ccd9dd6f3
Alb-22 <92102002+Alb-22@users.noreply.github.com>
2022-10-08 12:35:50 +0530

    Create savings.c

---
+++ savings.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+void main() {
+    
+   int n,j=0,i=0,sum=0,k,week=0;
+   printf("Input:");
+   scanf("%d",&n);
+   //printf("%d",n);
+   if(n<=5)
+   {
+       j=n;
+       while(j>0)
+       {
+             i=i+100;
+             sum=sum+i;
+             j--;
+       }
+   }
+   else
+   {
+       j=n;
+       while(j>0)
+       {
+           
+           if(j>7)
+                {
+                    k=5;
+                    while(k>0)
+                     {
+                            i=i+100;
+                            sum=sum+i;
+                            k--;
+                    }
+                    
+                    //end of a week
+                    j=j-7;
+                    week=week+1;
+                  
+                }
+            else
+               {
+                    if(j>5)
+                    {
+                
+                        k=5;
+                        while(k>0)
+                        {
+                            i=i+100;
+                            sum=sum+i;
+                            k--;
+                         }
+                         j=0;
+                    }
+                    {
+                        k=j%5;
+                        while(k>0)
+                        {
+                            i=i+100;
+                            sum=sum+i;
+                            k--;
+                         }
+                         j=0;
+                        
+                    }
+                   
+               }
+            // incrementing monday amount after week
+            i=100*week;
+    }
+       
+   }
+   
+
+    printf("Output:%d", sum);
+}
